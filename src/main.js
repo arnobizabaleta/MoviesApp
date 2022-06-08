bf6c4a42e5fd5cafc5d11e04ab7e806eb58ccab0
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-06-08 17:36:37 -0500

    Lista de películas de tendencia

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
-API_KEY="";
-console.log("API URL = https://jsjjjjhs/api_key=" + API_KEY);
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+    movies.forEach(movie => {
+        //Seleccionando al elemento con clase .trendingPreview-movieList dentro del elemento con id #trendingPreview
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+      
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container'); // Agrengando clase a movieContainer
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);//Definiendo atributo alt de movieImg
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  }
+  
+  getTrendingMoviesPreview();
